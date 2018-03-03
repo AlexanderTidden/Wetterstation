560096f4d9f1e5f935f20f96d2ab4b95fe08309c
AlexanderTidden <Alexander.Tidden@gmx.de>
2018-03-04 00:06:34 +0100

    update doc, add prototype code for BMP280 + OLED

---
+++ bmp280testDisplay/bmp280testDisplay.ino
@@ -0,0 +1,99 @@
+/***************************************************************************
+  This is a library for the BMP280 humidity, temperature & pressure sensor
+
+  Designed specifically to work with the Adafruit BMEP280 Breakout 
+  ----> http://www.adafruit.com/products/2651
+
+  These sensors use I2C or SPI to communicate, 2 or 4 pins are required 
+  to interface.
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit andopen-source hardware by purchasing products
+  from Adafruit!
+
+  Written by Limor Fried & Kevin Townsend for Adafruit Industries.  
+  BSD license, all text above must be included in any redistribution
+ ***************************************************************************/
+
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BMP280.h>
+
+#define BMP_SCK 13
+#define BMP_MISO 12
+#define BMP_MOSI 11 
+#define BMP_CS 10
+
+Adafruit_BMP280 bme; // I2C
+//Adafruit_BMP280 bme(BMP_CS); // hardware SPI
+//Adafruit_BMP280 bme(BMP_CS, BMP_MOSI, BMP_MISO,  BMP_SCK);
+
+/*********************************************************************/
+
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define OLED_RESET LED_BUILTIN //4 *no reset PIN on OLED display
+Adafruit_SSD1306 display(OLED_RESET);
+
+#if (SSD1306_LCDHEIGHT != 64)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
+
+/*******************************OLED**********************************/
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(F("BMP280 test"));
+  
+    // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
+  // init done
+  
+  // Show image buffer on the display hardware.
+  // Since the buffer is intialized with an Adafruit splashscreen
+  // internally, this will display the splashscreen.
+  display.display();
+  delay(1000);             // wait 2s
+  display.clearDisplay();  // Clear the buffer.
+  
+  if (!bme.begin()) {  
+    Serial.println("Could not find a valid BMP280 sensor, check wiring!");
+    while (1);
+  }
+}
+  
+void loop() {
+
+    float t = bme.readTemperature();
+    float p = bme.readPressure();
+    float a = bme.readAltitude(1013.25);
+    
+    Serial.print("Temperature = ");
+    Serial.print(t);
+    Serial.println(" *C");
+    
+    Serial.print("Pressure = ");
+    Serial.print(p);
+    Serial.println(" Pa");
+
+    Serial.print("Approx altitude = ");
+    Serial.print(a); // this should be adjusted to your local forcase
+    Serial.println(" m");
+
+    // display values  
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.setCursor(0,0);
+    display.println("Pressure: ");
+    display.print(p);
+    display.println(" %");
+    display.println("Temperature: ");
+    display.print(t);
+    display.println(" *C");
+    display.display();
+    
+    Serial.println();
+    delay(2000);
+}
